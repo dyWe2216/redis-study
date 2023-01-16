20a1a82ef1b7d0c400975543b05346c4cc2f7221
dyWe2216 <dnleodud@gmail.com>
2023-01-16 11:24:10 +0900

    add: redis pipeline test

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -2,10 +2,30 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
 	});
+	await client.hSet('car2', {
+		color: 'red',
+		year: 1951
+	});
+	await client.hSet('car3', {
+		color: 'red',
+		year: 1952
+	});
+	await client.hSet('car4', {
+		color: 'red',
+		year: 1953
+	});
+
+	const commands = [1, 2, 3, 4].map((id) => {
+		return client.hGetAll('car' + id);
+	});
+
+	const results = await Promise.all(commands);
+
+	console.log(results);
 
 	const car = await client.hGetAll('car');
 
