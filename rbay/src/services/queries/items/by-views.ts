6fe14536a0bfbcdb59e2aeb6224093144be9c445
dyWe2216 <dnleodud@gmail.com>
2023-01-18 09:12:31 +0900

    add: 아이템 정렬 함수 구현
    - redis sort를 통해 여러 필드 조회 및 정렬

--- rbay/src/services/queries/items/by-views.ts
+++ rbay/src/services/queries/items/by-views.ts
@@ -1,1 +1,34 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { itemsByViewsKey, itemsKey } from '$services/keys';
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	let results: any = await client.sort(itemsByViewsKey(), {
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
+		BY: 'noscore',
+		DIRECTION: order,
+		LIMIT: {
+			offset,
+			count
+		}
+	});
+
+	const items = [];
+
+	while (results.length) {
+		const [id, name, views, ...rest] = results;
+		const item = deserialize(id, { name, views });
+
+		items.push(item);
+		results = rest;
+	}
+
+	return items;
+};
