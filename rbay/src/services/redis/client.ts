a724838b3b1b0d85c548b1650494fe0dbb382080
dyWe2216 <dnleodud@gmail.com>
2023-01-20 18:06:50 +0900

    mod: 제품 상세 조회 시 조회 수 업데이트 로직 변경
    - lua script를 이용해 트랜잭션처리 하도록 로직 변경

--- rbay/src/services/redis/client.ts
+++ rbay/src/services/redis/client.ts
@@ -1,11 +1,57 @@
-import { createClient } from 'redis';
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from '$services/keys';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1, // 키 수
+			SCRIPT: `
+				return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+			`,
+			transformArguments(key: string, value: number) {
+				return [key, value.toString()];
+				// ['books:count', '5']
+				// EVALSHA <ID> 1 'books:count' '5'
+			},
+			transformReply(reply: any) {
+				return reply; // 명령어의 결과
+			}
+		}),
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+				local itemsViewsKey = KEYS[1]
+				local itemsKey = KEYS[2]
+				local itemsByViewsKey = KEYS[3]
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+
+				local inserted = redis.call('PFADD', itemsViewsKey, userId)
+
+				if inserted == 1 then
+					redis.call('HINCRBY', itemsKey, 'views', 1)
+					redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+				end
+			`,
+			transformArguments(itemId: string, userId: string) {
+				return [
+					itemsViewsKey(itemId), // items:views:asdf
+					itemsKey(itemId), // items:asdf
+					itemsByViewsKey(), // items:views
+					itemId, // asdf
+					userId // u1234
+				];
+
+				// EVALSHA ID 3 items:views:asdf items:asdf items:views asdf u1234
+			},
+			transformReply() {}
+		})
+	}
 });
 
 client.on('error', (err) => console.error(err));
